0a37675992985a740bab31e033570b7d84b6fd6c
flux <25628292+fluxionary@users.noreply.github.com>
2019-11-24 00:58:13 +0000

    adapt for blocky

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 local conf = {}
 
 -- set this to 'true' to make ingots 8 times larger
-conf.is_big = false
+conf.is_big = minetest.settings:get_bool("ingots.big", true)
 
 return conf
--- init.lua
+++ init.lua
@@ -24,6 +24,9 @@ dofile(minetest.get_modpath("ingots").."/api.lua")
 
 --Here you can make individual choices per ingot on which varian will be used.
 --To disable the ability of an ingot to be placed just comment out it's registration line.
+if minetest.get_modpath("basic_materials") then
+		ingots.register_ingots("basic_materials:brass_ingot", "ingot_brass.png", conf.is_big)
+end
 
 if minetest.get_modpath("default") then
 		ingots.register_ingots("default:copper_ingot", "ingot_copper.png", conf.is_big)
@@ -36,22 +39,25 @@ end
 if minetest.get_modpath("moreores") then
 		ingots.register_ingots("moreores:silver_ingot", "ingot_silver.png", conf.is_big)
 		ingots.register_ingots("moreores:mithril_ingot", "ingot_mithril.png", conf.is_big)
-		if not minetest.registered_items["default:tin_ingot"] then
-			ingots.register_ingots("moreores:tin_ingot", "ingot_tin.png", conf.is_big)
-		end
 end
 
-if minetest.get_modpath("technic") then
+if minetest.get_modpath("technic_worldgen") then
 		ingots.register_ingots("technic:stainless_steel_ingot", "ingot_stainless_steel.png", conf.is_big)
 		ingots.register_ingots("technic:carbon_steel_ingot", "ingot_carbon_steel.png", conf.is_big)
-		ingots.register_ingots("technic:mixed_metal_ingot", "ingot_mixed_metal.png", conf.is_big)
-		ingots.register_ingots("technic:brass_ingot", "ingot_brass.png", conf.is_big)
-		ingots.register_ingots("technic:lead_ingot", "ingot_lead.png", conf.is_big)
-		ingots.register_ingots("technic:zinc_ingot", "ingot_zinc.png", conf.is_big)
-		ingots.register_ingots("technic:chromium_ingot", "ingot_chromium.png", conf.is_big)
 		ingots.register_ingots("technic:cast_iron_ingot", "ingot_cast_iron.png", conf.is_big)
+
+		ingots.register_ingots("technic:chromium_ingot", "ingot_chromium.png", conf.is_big)
+		ingots.register_ingots("technic:lead_ingot", "ingot_lead.png", conf.is_big)
+		ingots.register_ingots("technic:uranium_ingot", "ingot_uranium.png", conf.is_big)
+		ingots.register_ingots("technic:zinc_ingot", "ingot_zinc.png", conf.is_big)
 end
 
-if minetest.get_modpath("rainbow_ore") then
-		ingots.register_ingots("rainbow_ore:rainbow_ore_ingot", "ingot_rainbow_ore.png", conf.is_big)
+if minetest.get_modpath("terumet") then
+		ingots.register_ingots("terumet:ingot_cgls", "ingot_coreglass.png", conf.is_big)
+		ingots.register_ingots("terumet:ingot_raw", "ingot_terumetal.png", conf.is_big)
+		ingots.register_ingots("terumet:ingot_tcha", "ingot_teruchalcum.png", conf.is_big)
+		ingots.register_ingots("terumet:ingot_tcop", "ingot_terucopper.png", conf.is_big)
+		ingots.register_ingots("terumet:ingot_tgol", "ingot_terugold.png", conf.is_big)
+		ingots.register_ingots("terumet:ingot_tste", "ingot_terusteel.png", conf.is_big)
+		ingots.register_ingots("terumet:ingot_ttin", "ingot_terutin.png", conf.is_big)
 end
